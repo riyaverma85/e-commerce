45d3defd130c16bf818a8eb9ff1628c8c98a2662
riyaverma85 <riyaverma2976@gmail.com>
2025-10-08 03:01:34 -0700

    css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Layout from "./Layout";
 import Home from "./pages/Home";
 import Shop from "./pages/Shop";
+import About from "./pages/About";
 
 
 const App=()=>{
@@ -14,7 +15,7 @@ const App=()=>{
                <Route index element={<Home/>}/>
                <Route path="/home" element={<Home/>}/>
                <Route path="/shop" element={<Shop/>}/>
-                
+                <Route path="/about" element={<About/>}/>
             </Route>
            </Routes>
         </BrowserRouter>
