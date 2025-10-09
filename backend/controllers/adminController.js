c51969670a96506940e50d923792846a9bba9791
riyaverma85 <riyaverma2976@gmail.com>
2025-10-08 18:04:47 -0700

    route

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,21 @@
+import Admin from "../models/Admin.js";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+export const loginAdmin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const admin = await Admin.findOne({ email });
+    if (!admin) return res.status(400).json({ message: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+    res.status(200).json({ message: "Login successful", token });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
