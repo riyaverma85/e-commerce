5247d20e723593565eb59598868741353ec9f66c
riyaverma85 <riyaverma2976@gmail.com>
2025-10-13 17:01:00 -0700

    asd

--- backend/app.js
+++ backend/app.js
@@ -18,5 +18,5 @@ app.get('/', (req, res) => res.send('API running'));
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log('Server running on', PORT));
