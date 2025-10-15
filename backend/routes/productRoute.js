0fdef6ae542c60ab7e301b23963d6e52dc1e5f44
riyaverma85 <riyaverma2976@gmail.com>
2025-10-14 22:24:19 -0700

    product

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,15 +1,3 @@
-// import express from "express";
-// import { addProduct, getProducts } from "../controllers/productController.js";
-// import { protect, admin } from "../middleware/authMiddleware.js"; // admin middleware for admin-only routes
-// import multer from "multer";
-
-// const router = express.Router();
-// const upload = multer({ dest: "uploads/" }); // temp folder for multer
-
-// router.post("/add", protect, admin, upload.single("image"), addProduct);
-// router.get("/", getProducts);
-
-// export default router;
 import express from "express";
 import { addProduct, getProducts } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js"; // admin middleware for admin-only routes
