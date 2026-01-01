1752c1cfcc22250d88081e25668a8988af7c6d7d
riyaverma85 <riyaverma2976@gmail.com>
2025-12-31 23:28:23 -0800

    js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,3 +1,16 @@
+// const mongoose = require("mongoose");
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.DBCON);
+//     console.log("✅ MongoDB connected successfully");
+//   } catch (err) {
+//     console.error("❌ MongoDB connection failed:", err.message);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
